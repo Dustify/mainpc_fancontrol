434319b3deb150afccbd3bfec7092d1d3ca8a955
Adrian Johnson <aj@dustify.com>
2018-10-21 00:52:49 +0100

    more commenting

--- src/PwmControl.h
+++ src/PwmControl.h
@@ -11,13 +11,16 @@ class PwmControl
   public:
     PwmControl(uint8_t timerNumber, uint16_t pwm_frequency)
     {
+        // get timer
         HardwareTimer timer(timerNumber);
 
+        // set timmr period
         timer.pause();
         timer.setPeriod(1e6 / pwm_frequency);
         timer.refresh();
         timer.resume();
 
+        // find adjusted max value
         this->pwm_max_value = (1098.649576562142 / pwm_frequency) * 65535;
     }
 
--- src/PwmOutput.h
+++ src/PwmOutput.h
@@ -10,47 +10,55 @@
 class PwmOutput
 {
   private:
+    // pwm pin
     uint8_t pin;
-    double min_factor, max_factor;
 
+    // pwm control and temp objects
     PwmControl *pwmControl;
     Temperature *temperature;
 
+    // min and max values
     uint16_t min_value, max_value;
 
+    // current value
     uint16_t value;
 
+    // counts and counter for working out when to change value
     uint16_t counts, counter;
 
   public:
     PwmOutput(uint8_t pin, double min_factor, double max_factor, uint16_t ticks_per_second, PwmControl *pwmControl, Temperature *temperature)
     {
+        // set values locally as requried
         this->pin = pin;
-        this->min_factor = min_factor;
-        this->max_factor = max_factor;
         this->pwmControl = pwmControl;
         this->temperature = temperature;
 
-        this->min_value = pwmControl->getPwmMaxValue() * this->min_factor;
-        this->max_value = pwmControl->getPwmMaxValue() * this->max_factor;
+        this->min_value = pwmControl->getPwmMaxValue() * min_factor;
+        this->max_value = pwmControl->getPwmMaxValue() * max_factor;
 
         this->counts = ticks_per_second / UPDATES_PER_SECOND;
         this->counter = this->counts;
 
+        // set pin mode
         pinMode(this->pin, PWM);
     }
 
     void Tick()
     {
+        // increment counter
         counter++;
 
+        // if counter has not 'elapsed' just continue
         if (counter < counts)
         {
             return;
         }
 
+        // counter has elapsed...
         counter = 0;
 
+        // set value
         value =
             map(temperature->getTempFactorFull(),
                 0,
@@ -58,6 +66,7 @@ class PwmOutput
                 min_value,
                 max_value);
 
+        // write value to pwm pin
         pwmWrite(pin, value);
     }
 
